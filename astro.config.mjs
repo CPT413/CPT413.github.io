ff418d1fe663b81a05313fa307bb5a324ec02893
CPT413 <cameron.thompson413@gmail.com>
2025-07-08 14:39:37 -0400

    site setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
-  base: 'my-repo',
+    site: 'https://cpt413.github.io',
+  base: 'cpt413.github.io',
 });
