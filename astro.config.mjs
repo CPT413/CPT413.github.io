222a8eac91fefe334ee07f8b4eac981a0f676501
CPT413 <cameron.thompson413@gmail.com>
2025-07-08 22:06:46 -0400

    Website demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://cpt413.github.io',
+    vite: {
+        ssr: {
+            noExternal: [/^webcoreui/]
+        }
+    }
 });
